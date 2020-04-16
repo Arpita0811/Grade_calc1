0d6c1d72fd87709d203e9765b7c0acd933a6ce80
Arpita0811 <63471471+Arpita0811@users.noreply.github.com>
2020-04-16 22:44:45 +0530

    Create Grade1.c
    Calculates the Grade of Student based on the marks entered by the user.

---
+++ Grade1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main() 
+{
+    int n;
+    printf("Enter the Marks of the Student\n");
+    scanf("%d",&n);
+    if(n>=85 && n<=100)
+    {
+        printf("Grade A");
+    }
+    else if(n>=70 && n<=84)
+    {
+        printf("Grade B");
+     
+    }
+    else if(n>=55 && n<=69)
+    {
+        printf("Grade C");
+    }
+    else if(n>=40 && n<=54)
+    {
+        printf("Grade D");
+    }
+    else
+    {
+        printf("Grade F");
+    }
+ }
+    
